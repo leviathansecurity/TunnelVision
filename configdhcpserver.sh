a9b15fef926b072fe2042beace1c97fa9eb90349
lsg-MorattiSec <163063359+lsg-MorattiSec@users.noreply.github.com>
2024-04-13 18:57:54 -0700

    Update range
    When using a bridged adapter, it's possible that the router gives our DHCP server 192.168.1.0/24 ranges. Modifying to 10.13.37.0/24 instead

--- configdhcpserver.sh
+++ configdhcpserver.sh
@@ -12,13 +12,13 @@ echo "Creating /etc/dhcp/dhcpd-noroute.conf and /etc/dhcp/dhcpd-route.conf..."
 touch /etc/dhcp/dhcpd-noroute.conf
 
 echo "Adding content to dhcpd-noroute.conf..."
-printf "# dhcpd.conf\nauthoritative;\noption rfc3442  code 121 = array of integer 8;\noption ms-rfc3442 code 249 = array of integer 8;\n\n subnet 192.168.1.0 netmask 255.255.255.0 {\nrange 192.168.1.10 192.168.1.239;\noption domain-name-servers 8.8.8.8;\n option subnet-mask 255.255.255.0;\noption routers 192.168.1.1;\noption broadcast-address 192.168.1.255;\ndefault-lease-time 30;\nmax-lease-time 30;\n} " > /etc/dhcp/dhcpd-noroute.conf
+printf "# dhcpd.conf\nauthoritative;\noption rfc3442  code 121 = array of integer 8;\noption ms-rfc3442 code 249 = array of integer 8;\n\n subnet 10.13.37.0 netmask 255.255.255.0 {\nrange 10.13.37.10 10.13.37.239;\noption domain-name-servers 8.8.8.8;\n option subnet-mask 255.255.255.0;\noption routers 10.13.37.1;\noption broadcast-address 10.13.37.255;\ndefault-lease-time 30;\nmax-lease-time 30;\n} " > /etc/dhcp/dhcpd-noroute.conf
 
 echo "Creating dhcpd-route.conf..."
 touch /etc/dhcp/dhcpd-route.conf
 
 echo "Adding content to dhcpd-route.conf..."
-printf "# dhcpd.conf\nauthoritative;\noption rfc3442  code 121 = array of integer 8;\noption ms-rfc3442 code 249 = array of integer 8;\n\n subnet 192.168.1.0 netmask 255.255.255.0 {\nrange 192.168.1.10 192.168.1.239;\noption domain-name-servers 8.8.8.8;\noption subnet-mask 255.255.255.0;\noption routers 192.168.1.1;\noption broadcast-address 192.168.1.255;\ndefault-lease-time 30;\nmax-lease-time 30;\noption rfc3442  32, 8, 8, 8, 8, 192, 168, 1, 1;\noption ms-rfc3442 32, 8, 8, 8, 8, 192, 168, 1, 1;\n} " > /etc/dhcp/dhcpd-route.conf
+printf "# dhcpd.conf\nauthoritative;\noption rfc3442  code 121 = array of integer 8;\noption ms-rfc3442 code 249 = array of integer 8;\n\n subnet 10.13.37.0 netmask 255.255.255.0 {\nrange 10.13.37.10 10.13.37.239;\noption domain-name-servers 8.8.8.8;\noption subnet-mask 255.255.255.0;\noption routers 10.13.37.1;\noption broadcast-address 10.13.37.255;\ndefault-lease-time 30;\nmax-lease-time 30;\noption rfc3442  32, 8, 8, 8, 8, 192, 168, 1, 1;\noption ms-rfc3442 32, 8, 8, 8, 8, 192, 168, 1, 1;\n} " > /etc/dhcp/dhcpd-route.conf
 
 echo "Replacing /etc/dhcp/dhcpd.conf with no route push config..."
 cp /etc/dhcp/dhcpd-noroute.conf /etc/dhcp/dhcpd.conf
